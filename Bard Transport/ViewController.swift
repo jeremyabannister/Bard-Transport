5b438e34c592bf3f4c4197289ff693b7437ccba0
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2016-05-14 15:03:44 -0400

    Initial Commit

---
+++ Bard Transport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bard Transport
+//
+//  Created by Jeremy Bannister on 5/14/16.
+//  Copyright © 2016 Jeremy Bannister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
